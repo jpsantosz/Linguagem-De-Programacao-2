62c65e2ab5f5db53e19f5e2f76ab2ecf376ac768
João Pedro Santos <124943793+jpsantosz@users.noreply.github.com>
2023-10-11 10:23:47 -0300

    Update Triangulo.java

--- ListaDeExercícios2/exercicio3/Triangulo.java
+++ ListaDeExercícios2/exercicio3/Triangulo.java
@@ -1,3 +1,8 @@
+//Crie um novo projeto que defina uma classe para representar um triângulo. Três atributos precisam ser definidos: l1, l2 e l3. Cada atributo representa um dos lados do triângulo, do tipo inteiro. 
+//A classe deve implementar construtores (padrão e completo), os métodos de acesso aos atributos (getters e setters), além de definir um método para informar o
+//tipo do triângulo (equilátero, isósceles ou escaleno) e outro para exibir os dados do triângulo. Em seguida, crie uma classe contendo o método main e instancie dois triângulos. 
+//Exiba os dados de cada triângulo, bem como o seu tipo.
+
 package ListaDeExercícios2.exercicio3;
 
 public class Triangulo {
