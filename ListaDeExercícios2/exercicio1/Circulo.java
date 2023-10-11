edfbcf493294ee77c503db5413704890c42f404c
João Pedro Santos <124943793+jpsantosz@users.noreply.github.com>
2023-10-11 10:27:14 -0300

    Update Circulo.java

--- ListaDeExercícios2/exercicio1/Circulo.java
+++ ListaDeExercícios2/exercicio1/Circulo.java
@@ -1,3 +1,7 @@
+//Crie um novo projeto que defina uma classe para representar um círculo. Dois atributos precisam ser definidos: raio, que é do tipo double e possui como valor default 1 e cor, que armazenada um valor do tipo String
+//e tem como valor default “vermelho”. A classe deve implementar os métodos de acesso aos atributos (getters e setters), além de definir um método para calcular a área do círculo (2 ∗ 𝜋 ∗ r^2) e outro para exibir os
+//dados do círculo. Em seguida, crie uma classe contendo o método main e instancie dois círculos. Altere a cor e o raio dos círculos, calcule a área e exiba seus dados.
+
 package ListaDeExercícios2.exercicio1;
 
 public class Circulo {
