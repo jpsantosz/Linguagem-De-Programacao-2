2549fdb07832807ff419de3398b49dcbe089b1f4
João Pedro Santos <124943793+jpsantosz@users.noreply.github.com>
2023-10-11 10:24:57 -0300

    Update principal.java

--- ListaDeExercícios2/exercicio2/principal.java
+++ ListaDeExercícios2/exercicio2/principal.java
@@ -1,3 +1,7 @@
+//Crie uma classe para representar um jogador de futebol, com os atributos nome, posição, nacionalidade, altura e peso. Crie os métodos de acesso aos atributos (setters e getters) e também um método para imprimir
+//todos os dados do jogador. Além disso, defina um método para calcular o IMC do jogador. Em seguida, crie uma classe com o método main utilizando array de objetos para armazenar os dados de até 40 jogadores, de acordo
+//com o desejo do usuário. Ao final, o programa deve exibir todos os dados dos jogadores, bem como o IMC de cada um deles.
+
 package ListaDeExercícios2.exercicio2;
 
 import java.util.Scanner;
